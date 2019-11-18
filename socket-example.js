e5b1580e2b767caa50e71104f1b23cdc5e9c97d0
vilnyus <govagharsh@gmail.com>
2019-11-18 21:33:14 +0400

    minor

--- socket-example.js
+++ socket-example.js
@@ -4,15 +4,13 @@ var io = require('socket.io')(server);
 
 server.listen(8080);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function(data){
-    console.log(data);
+  socket.on('my other event', function(data) {
+    console.log(data, 'This is from socket.on JS');
   });
 });
-
-

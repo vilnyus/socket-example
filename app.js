62f1fee8e211e54b86b3f92c9dfb8a38683bc9d2
vilnyus <govagharsh@gmail.com>
2019-11-20 16:30:30 +0400

    sockets added

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
 app.get('/', function(req, res){
-    res.sendFile('D:/vscode/nodejs-starter/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
+var client = 0;
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.broadcast.emit('hi');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+  client++;
+  io.emit('newclientconnect', {message: ` ${client} connected`});
+
+  socket.on('chat message', function(msg){
+      console.log("chat received");
+      io.emit('chat message', msg);
     });
-    socket.on('chat message', function(msg){
-        io.emit('chat message', function(msg){
-            io.emit('chat message', msg);
-        });
-        console.log('message: ' + msg);
-    })
+  });
+
+http.listen(3000, function(){
+    console.log("Listening on port: 3000");
 });
 
-http.listen(3030, function(){
-    console.log('listen on *:3030');
-});
--- socket-example.js
+++ socket-example.js
@@ -1,18 +1,31 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var io = require('socket.io');
 
-server.listen(8080);
+// server.listen(8080);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function(data){
-    console.log(data, "This is from socket.on JS");
+var chat = io
+  .of('/chat')
+  .on('connection', function (socket) {
+    socket.emit('a message', {
+        that: 'only'
+      , '/chat': 'will get'
+    });
+    chat.emit('a message', {
+        everyone: 'in'
+      , '/chat': 'will get'
+    });
   });
-});
+
+var news = io
+  .of('/news')
+  .on('connection', function (socket) {
+    socket.emit('item', { news: 'item' });
+  });
 
 

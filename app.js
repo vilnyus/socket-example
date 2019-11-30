25534821597af5ce12ab87b841393e8305355b39
vilnyus <govagharsh@gmail.com>
2019-11-30 11:33:35 +0400

    working multiple rooms

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(server);
 const port = 3000;
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
@@ -12,25 +12,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/javascript', (req, res) => {
-  res.sendFile('/javascript', (err)=>{
-    console.log(err);
-  });
-  // res.sendFile('/public/javascript.html');
+  console.log('this is from app.js');
+  res.sendFile(__dirname + '/public/javascript.html');
 });
 
 const tech = io.of('/tech');
 
-tech.on('connection', (socket)=>{
-  socket.on('join', (data)=>{
+tech.on('connection', socket => {
+  socket.on('join', data => {
     socket.join(data.room);
     tech.in(data.room).emit('message', `new user connected ${data.room} room`);
   });
 
-  socket.on('message', (data)=>{
+  socket.on('message', data => {
     tech.in(data.room).emit('message', data.msg);
   });
 
-  socket.on('disconnect', ()=>{
-    tech.emit('message', "user disconnected")
+  socket.on('disconnect', () => {
+    tech.emit('message', 'user disconnected');
   });
-})
+});
